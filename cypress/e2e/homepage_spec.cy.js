6f5cc31618d10ff6b9979a9e81e4b81f5253f186
Karrar Qasim <Karawr1996@gmail.com>
2023-03-07 10:24:11 -0700

    Added more tests

--- cypress/e2e/homepage_spec.cy.js
+++ cypress/e2e/homepage_spec.cy.js
@@ -6,7 +6,20 @@ describe('Homepage flow', () => {
         'response.ok'
       })
     }) 
-    it('user should see a homepage with title', () => {
-      cy.get('h1')
-    })
+    it('user should see a homepage with title and shortened urls', () => {
+        cy.get('h1')
+        cy.get('.url').find('h3').contains('Awesome photo')
+        cy.get('.url').find('a').contains('http://localhost:3001/useshorturl/1')
+        cy.get('.url').find('p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+    
+      })
+      it('user can view the form with proper inputs', () => {
+        cy.get('[placeholder="Title..."]').should('exist')
+        cy.get('[placeholder="URL to Shorten..."]').should('exist')
+        cy.get('[placeholder="URL to Shorten..."]').should('exist')
+      })
+      it('user can fill out the form, information is reflected in the input fields', () => {
+        cy.get('[placeholder="Title..."]').type('Karrar The Great')
+        cy.get('[placeholder="URL to Shorten..."]').type('http://TheManTheMythTheLegenddddKarrrrrrarrrrr.org/#1')
+      })
   })
