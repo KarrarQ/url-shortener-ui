11e9b0841290aa7cd4a8227a604415ca67851b59
Karrar Qasim <Karawr1996@gmail.com>
2023-03-07 10:48:34 -0700

    Uploaded a new image of the goat

--- cypress/e2e/post_spec.cy.js
+++ cypress/e2e/post_spec.cy.js
@@ -9,13 +9,13 @@ describe("post flow", () => {
       cy.visit("http://localhost:3000/");
     });
     it('user can fill out the form, submit and view the card on the homepage', () => {
-      cy.get('[placeholder="Title..."]').type('Karrar')
-        .get('[placeholder="URL to Shorten..."]').type('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      cy.get('[placeholder="Title..."]').type('Messi')
+        .get('[placeholder="URL to Shorten..."]').type('https://www.planetsport.com/image-library/square/1200/a/argentinas-lionel-messi-poses-with-the-fifa-world-cup-trophy.jpg')
         .get('button').click()
         .get('.url').last()
-      cy.get('section > :nth-child(2)').get('section > :nth-child(2)').contains('Karrar')
+      cy.get('section > :nth-child(2)').get('section > :nth-child(2)').contains('Messi')
       cy.get('section > :nth-child(2)').get('section > :nth-child(2)').contains('http://localhost:3001/useshorturl/2')
-      cy.get('section > :nth-child(2)').get(':nth-child(2) > p').contains('https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+      cy.get('section > :nth-child(2)').get(':nth-child(2) > p').contains('https://www.planetsport.com/image-library/square/1200/a/argentinas-lionel-messi-poses-with-the-fifa-world-cup-trophy.jpg')
   
     })
   })
